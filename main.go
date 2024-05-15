d4448941385f5290c472efca4f5e388400290f40
Hendrik Maier <hendrik_maier@protonmail.com>
2024-05-15 10:57:36 +0200

    refactor: make array of structs global

--- main.go
+++ main.go
@@ -7,57 +7,45 @@ import (
     "flag"
 )
 
-func main(){
-    
-    type Aktion struct {
-        Name string
-        Verteidigungen []string
-    }
+type Aktion struct {
+    Name string
+    Verteidigungen []string
+}
 
-    var aktionen = []Aktion{
-        { 
-            Name: "Jab",
-            Verteidigungen: []string{"Doppeldeckung", "Winkekatze"},
-        },
-        { 
-            Name: "Punch",
-            Verteidigungen: []string{"Doppeldeckung", "Block"},
-        },
-        { 
-            Name: "hinterer Push-Kick",
-            Verteidigungen: []string{"Parieren", "Wegwischen", "Ausweichen"},
-        },
-        { 
-            Name: "vorderer Push-Kick",
-            Verteidigungen: []string{"Parieren", "Wegwischen", "Ausweichen"},
-        },
-        { 
-            Name: "vorderer Haken",
-            Verteidigungen: []string{"Block"},
-        },
-        { 
-            Name: "hinterer Haken",
-            Verteidigungen: []string{"Block"},
-        },
-        { 
-            Name: "vorderer Ribkick",
-            Verteidigungen: []string{"Dreipunkt-Block", "Schienbein"},
-        },
-        { 
-            Name: "hinterer Ribkick",
-            Verteidigungen: []string{"Dreipunkt-Block", "Schienbein"},
-        },
-    }
-
-    for {
-        sleeping := rand.Intn(4)+1
-        time.Sleep(time.Second * time.Duration(sleeping))
-        aktion := aktionen[rand.Intn(len(aktionen))]
-        fmt.Println("Verteidige ",  aktion.Name, " mit ", aktion.Verteidigungen[rand.Intn(len(aktion.Verteidigungen))])
-    }
-
-
-
+var aktionen = []Aktion{
+    { 
+        Name: "Jab",
+        Verteidigungen: []string{"Doppeldeckung", "Winkekatze"},
+    },
+    { 
+        Name: "Punch",
+        Verteidigungen: []string{"Doppeldeckung", "Block"},
+    },
+    { 
+        Name: "hinterer Push-Kick",
+        Verteidigungen: []string{"Parieren", "Wegwischen", "Ausweichen"},
+    },
+    { 
+        Name: "vorderer Push-Kick",
+        Verteidigungen: []string{"Parieren", "Wegwischen", "Ausweichen"},
+    },
+    { 
+        Name: "vorderer Haken",
+        Verteidigungen: []string{"Block"},
+    },
+    { 
+        Name: "hinterer Haken",
+        Verteidigungen: []string{"Block"},
+    },
+    { 
+        Name: "vorderer Ribkick",
+        Verteidigungen: []string{"Dreipunkt-Block", "Schienbein"},
+    },
+    { 
+        Name: "hinterer Ribkick",
+        Verteidigungen: []string{"Dreipunkt-Block", "Schienbein"},
+    },
+}
 
 var maximum int
 
